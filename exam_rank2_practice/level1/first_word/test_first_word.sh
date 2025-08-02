1c2fae5a34db2c779a6d1a8d488d639991603cb7
msedeno- <msedeno-@student.42malaga.com>
2025-08-02 19:14:38 +0200

    02 AUG 25 19:14

---
+++ exam_rank2_practice/level1/first_word/test_first_word.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./a.out "	hola mundo"
+./a.out "palabra"
+./a.out " 	primera palabra"
+./a.out "		palabra otra"
+./a.out ""
+./a.out
+./a.out "hello@world again"
---
+++ rank2_practice/exam/rank2/level4/flood_fill.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flood_fill.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msedeno- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/02 14:53:57 by msedeno-          #+#    #+#             */
+/*   Updated: 2025/08/02 16:48:56 by msedeno-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "test_functions.h"
+void	fill(char **tab, t_point size, char target, int row, int col, )
+
+void	flood_fill(char **tab, t_point size, t_point begin)
