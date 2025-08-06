075b3f3af90ade162c9b716f513afc464f36d1ca
msedeno- <msedeno-@student.42malaga.com>
2025-08-06 16:38:59 +0200

    exs repaso

--- rank2_practice/exam/rank2/rstr_capitalizer.c
+++ rank2_practice/exam/rank2/rstr_capitalizer.c
@@ -1,73 +1,85 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   str_capitalizer.c                                  :+:      :+:    :+:   */
+/*   rstr_capitalizer.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: casimarasn <casimarasn@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/05 13:46:55 by msedeno-          #+#    #+#             */
-/*   Updated: 2025/08/06 16:35:37 by casimarasn       ###   ########.fr       */
+/*   Created: 2025/08/06 15:51:58 by casimarasn        #+#    #+#             */
+/*   Updated: 2025/08/06 16:38:16 by casimarasn       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+int	ft_isspace(char c)
+{
+	return (c == ' ' || c == '\t');
+}
+
 char	ft_tolower(char c)
 {
-	if(c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		c += 32;
 	return (c);
 }
 
-
 char	ft_toupper(char c)
 {
-	if (c >= 'a' && c <='z')
+	if (c >= 'a' && c <= 'z')
 		c -= 32;
 	return (c);
 }
 
-
-int	ft_isspace(char c)
+int	ft_strlen(char *s)
 {
-	return (c == ' '|| c == '\t');
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
-
-char	*str_capitalizer(char *str)
+void	rstr_capitalizer(char *str)
 {
-	int	i = 0;
-	int	new_word = 1;
+	int	len;
+	int	end_word;
+	int	i;
 
-	while (str[i])
+	len = ft_strlen(str) - 1;
+	end_word = 1;
+	while (str[len])
 	{
-		str[i] = ft_tolower(str[i]);
-		if (((new_word) && ft_tolower(str[i])))
-			str[i] = ft_toupper(str[i]);
-		if (ft_isspace(str[i]))
-			new_word = 1;
+		str[len] = ft_tolower(str[len]);
+		if (str[len] && (end_word))
+			str[len] = ft_toupper(str[len]);
+		if (ft_isspace(str[len]))
+			end_word = 1;
 		else
-			new_word = 0;
-		write (1, &str[i], 1);
-		i++;
+			end_word = 0;
+		len--;
 	}
-	return(str);
+	i = 0;
+	while (str[i])
+		write (1, &str[i++], 1);
 }
 
 int	main(int ac, char **av)
 {
-	int	i = 1;
+	int	i;
 
+	i = 1;
 	if (ac >= 2)
 	{
 		while (av[i])
 		{
-			str_capitalizer(av[i]);
-			write(1, "\n", 1);
+			rstr_capitalizer(av[i]);
+			write (1, "\n", 1);
 			i++;
 		}
 	}
 	else
-		write(1, "\n",1);
-	return(0);
+		write (1, "\n", 1);
+	return (0);
 }
