edc671c64c9f4189f89e7ff16f63a77da6f2da04
msedeno- <msedeno-@student.42malaga.com>
2025-11-22 19:52:11 +0100

    22 ^b 25 19:52

--- EXAM/RANK2/level4/sort_in_tab/sort_in_tab.c
+++ EXAM/RANK2/level4/sort_in_tab/sort_in_tab.c
@@ -6,11 +6,33 @@
 /*   By: casimarasn <casimarasn@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 17:52:42 by casimarasn        #+#    #+#             */
-/*   Updated: 2025/07/30 18:24:31 by casimarasn       ###   ########.fr       */
+/*   Updated: 2025/11/22 19:46:47 by casimarasn       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+
+/*void	sort(char *str, int n)
+{
+	int		i;
+	char	tmp;
+
+	while (n > 0)
+	{
+		i = 0;
+		while (i < n - 1)
+		{
+			if (str[i] > str[i + 1])
+			{
+				tmp = str[i];
+				str[i] = str[i + 1];
+				str[i + 1] = tmp;
+			}
+			i++;
+		}
+		n--;
+	}
+}*/
 
 void	sort_in_tab(int *tab, unsigned int size)
 {
@@ -18,7 +40,7 @@ void	sort_in_tab(int *tab, unsigned int size)
 	unsigned int	i;
 
 	i = 0;
-	while (i < size)
+	while (i < size - 1)
 	{
 		if (tab[i] > tab[i + 1])
 		{
